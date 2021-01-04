54b1dd98c56151658ae4ffb6abe5bc264bbf2d39
angelclone2 <angelclone2@gmail.com>
2021-01-04 19:20:09 +0100

    3.2 hecho

--- src/Controller/ContactoController.php
+++ src/Controller/ContactoController.php
@@ -26,7 +26,7 @@ class ContactoController extends AbstractController
         "telefono" => "965929190", "email" => "mario.mont@gmail.com"),
         array("codigo" => 4, "nombre" => "Laura Martínez",
         "telefono" => "611223344", "email" => "lm2000@gmail.com"),
-        array("codigo" => 5, "nombre" => "Nora Jover",
+        //array("codigo" => 5, "nombre" => "Nora Jover",
         "telefono" => "638765432", "email" => "norajover@hotmail.com"),
         );*/
 
